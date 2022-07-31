bf99d3564963dbad4932a2020e11ed6e6794137f
rohshar6 <rohshar6@cisco.com>
2022-07-31 21:36:28 +0530

    arrays practice-1

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -16,3 +16,10 @@ var newElements = elements.map({
 newElements.forEach { ele in
     print(ele.value)
 }
+
+// flatmap() -> it just flattens the array - [[1,2], [3,4]]  -> [1,2,3,4]
+// compactMap() -> it safely unwraps arrays of optionals and removes nil element from the array and returns array with non-optionals
+
+
+let arr = [1, 4, 9, 13, 112]
+let sum = arr.reduce(0) {$0 + $1} // 139
