4e6f57bec604d8c4b46a4ccdd73d9cac302ccf4f
rohshar6 <rohshar6@cisco.com>
2022-08-01 23:08:01 +0530

    1st august

---
+++ Containers.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// array
+
+let array = ["a", "b"]
+
+// NSArray
+let nsArray = NSArray(array: array)
+
+// NSDictionary
+let nsDictionary = NSDictionary()
+
+// NSSet
+let nsSet = NSSet(array: array)
+print(nsSet)
