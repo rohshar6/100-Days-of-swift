b107c63818b471ae71b574e1cc6d7e40083242b7
rohshar6 <rohshar6@cisco.com>
2022-08-07 16:40:25 +0530

    set topic in-progress

---
+++ Set.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+var setConatiner: Set<Int> = [1,2,3,4,5]
+
+print(setConatiner)
+
+setConatiner.contains(1)
+setConatiner.contains(100)
+
+
+// Option Set
+
+//var setTwo: OptionSet = [] mainly used for bits
+
+// OrderedSet and OrderedDictionary
+var orderedSetElement = NSOrderedSet(array: [])
